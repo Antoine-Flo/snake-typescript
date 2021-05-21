29d85684685ad4cc32d6276a3b0b4b2ebba4ac42
Antoine-Flo <antoine.flouzat@gmail.com>
2021-05-21 20:33:41 +0200

    snake movement

--- app.js
+++ app.js
@@ -1,35 +1,88 @@
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
-var fpsInterval, now, then, interval;
-function startTick(fps) {
-    fpsInterval = 1000 / fps;
-    then = performance.now();
-    tick();
-}
-function tick() {
-    requestAnimationFrame(tick);
-    now = performance.now();
-    interval = now - then;
-    if (interval > fpsInterval) {
-        then = now - (interval % fpsInterval);
-        // update();
-        // draw();
-    }
-}
-// startTick(1)
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
+c.fillStyle = "black";
+c.fillRect(0, 0, canvas.width, canvas.height);
+var grid = 20;
 var Prize = /** @class */ (function () {
     function Prize() {
+        this.px = this.pickRandom();
+        this.py = this.pickRandom();
     }
+    Prize.prototype.pickRandom = function () {
+        return Math.floor(Math.random() * grid);
+    };
+    Prize.prototype.update = function () {
+        this.px += this.pickRandom();
+        this.py += this.pickRandom();
+    };
+    Prize.prototype.draw = function () {
+        c.fillStyle = "green";
+        c.fillRect(this.px * grid, this.py * grid, grid - 2, grid - 2);
+    };
     return Prize;
 }());
 var Snake = /** @class */ (function () {
     function Snake() {
+        var _this = this;
+        this.sx = 0;
+        this.sy = 0;
+        this.trail = [];
+        this.tail = 5;
+        this.draw = function () {
+            c.fillStyle = "red";
+            c.fillRect(_this.sx, _this.sy, grid - 2, grid - 2);
+        };
+        this.keyPush = function (evt) {
+            switch (evt.key) {
+                case "ArrowLeft":
+                    _this.sx -= 1;
+                    console.log(_this.sx);
+                    break;
+                case "ArrowDown":
+                    _this.sy += 1;
+                    break;
+                case "ArrowRight":
+                    _this.sx += 1;
+                    break;
+                case "ArrowUp":
+                    _this.sy -= 1;
+                    break;
+            }
+        };
     }
-    Snake.prototype.draw = function (x, y, color) {
-        c.fillStyle = color;
-        c.fillRect(x, y, 100, 100);
-    };
     return Snake;
 }());
-var snake = new Snake();
-snake.draw(10, 10, "red");
+var GameLoop = /** @class */ (function () {
+    function GameLoop() {
+        this.fpsInterval = 0;
+        this.now = 0;
+        this.then = 0;
+        this.interval = 0;
+        this.snake = new Snake();
+        this.prize = new Prize();
+    }
+    GameLoop.prototype.startTick = function (fps) {
+        this.fpsInterval = 1000 / fps;
+        this.then = performance.now();
+        this.tick();
+    };
+    GameLoop.prototype.tick = function () {
+        var _this = this;
+        requestAnimationFrame(function () { return _this.tick(); });
+        this.now = performance.now();
+        this.interval = this.now - this.then;
+        if (this.interval > this.fpsInterval) {
+            this.then = this.now - (this.interval % this.fpsInterval);
+            c.fillStyle = "black";
+            c.fillRect(0, 0, canvas.width, canvas.height);
+            document.addEventListener("keydown", this.snake.keyPush);
+            console.log(this.snake.sx);
+            console.log(this.snake.sy);
+            this.snake.draw();
+            this.prize.draw();
+        }
+    };
+    return GameLoop;
+}());
+var game = new GameLoop;
+// game.startTick(10);
--- app.ts
+++ app.ts
@@ -1,43 +1,102 @@
 
-const canvas = <HTMLCanvasElement>document.querySelector('canvas');
-const c = <CanvasRenderingContext2D>canvas.getContext('2d');
+const canvas = <HTMLCanvasElement>document.querySelector("canvas");
+const c = <CanvasRenderingContext2D>canvas.getContext("2d");
 
+c.fillStyle = "black";
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-let fpsInterval:number, now:number, then:number, interval:number;
-
-function startTick(fps:number) {
-    fpsInterval = 1000 / fps;
-    then = performance.now();
-    tick();
-}
-
-function tick() {
-
-    requestAnimationFrame(tick);
-    now = performance.now();
-    interval = now - then;
-
-    if(interval > fpsInterval) {
-        then = now - (interval % fpsInterval);
-
-        // update();
-        // draw();
-    }
-}
-// startTick(1)
+let grid = 20;
 
 class Prize {
+    px = this.pickRandom();
+    py = this.pickRandom();
+    
+    pickRandom() {
+       return Math.floor(Math.random() * grid);
+    }
 
+    update() {
+        this.px += this.pickRandom();
+        this.py += this.pickRandom();
+    }
+
+    draw() {
+      c.fillStyle = "green";
+      c.fillRect(this.px * grid, this.py * grid, grid - 2, grid - 2);
+    }
 }
 
 class Snake {
+    sx = 0;
+    sy = 0;
+    trail = [];
+    tail = 5;
 
 
-    draw(x:number, y:number, color:string) {
-        c.fillStyle = color;
-        c.fillRect(x, y, 100, 100)
+    draw = () => {
+      c.fillStyle = "red";
+      c.fillRect(this.sx, this.sy, grid - 2, grid - 2);
     }
+
+    keyPush = (evt: KeyboardEvent) => {
+
+        switch(evt.key) {
+            case "ArrowLeft":
+                this.sx -= 1;
+                console.log(this.sx);
+                break;
+
+            case "ArrowDown":
+                this.sy += 1;
+                break;
+
+            case "ArrowRight":
+                this.sx += 1;
+                break;
+
+            case "ArrowUp":
+                this.sy -= 1;
+                break;
+        }
+    }
+
 }
 
-const snake = new Snake()
-snake.draw(10, 10, "red")
+class GameLoop {
+  fpsInterval: number = 0;
+  now: number = 0;
+  then: number = 0;
+  interval: number = 0;
+  snake = new Snake();
+  prize = new Prize();
+
+  startTick(fps: number) {
+    this.fpsInterval = 1000 / fps;
+    this.then = performance.now();
+    this.tick();
+  }
+
+  tick() {
+    requestAnimationFrame(() => this.tick());
+    this.now = performance.now();
+    this.interval = this.now - this.then;
+
+    if (this.interval > this.fpsInterval) {
+      this.then = this.now - (this.interval % this.fpsInterval);
+      c.fillStyle = "black";
+      c.fillRect(0, 0, canvas.width, canvas.height);
+      document.addEventListener("keydown", this.snake.keyPush);
+
+
+      console.log(this.snake.sx);
+      console.log(this.snake.sy);
+      this.snake.draw();
+      this.prize.draw();
+
+    }
+  }
+}
+
+const game = new GameLoop;
+// game.startTick(10);
+
